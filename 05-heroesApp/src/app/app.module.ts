67f1a64fb73712cd75b4fbfc1933ba11a3d61a8e
Boris Caiza <77301967+BorisCaiza@users.noreply.github.com>
2022-04-06 16:09:04 -0500

    heroes

--- 05-heroesApp/src/app/app.module.ts
+++ 05-heroesApp/src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ErrorPageComponent
+ 
    
     ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ErrorPageComponent
+    HttpClientModule
+    
   
   ],
   providers: [],
---
+++ 05-heroesApp/src/app/heroes/components/heroe-tarjeta-component/heroe-tarjeta-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Heroe } from '../../pages/heroe/interfaces/heroes.interfaces';
+
+@Component({
+  selector: 'app-heroe-tarjeta-component',
+  templateUrl: './heroe-tarjeta-component.component.html',
+  styles: [
+  ]
+})
+export class HeroeTarjetaComponentComponent  {
+
+  @Input() heroe!: Heroe;
+  constructor() { }
+
+ 
+
+}
